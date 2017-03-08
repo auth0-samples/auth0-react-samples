5c856e33f0e4c8c9fd9b1b3011b0bf1b59263ccf
Ryan Chenkie <ryanchenkie@gmail.com>
2017-03-08 09:21:31 -0500

    remove auth0 config

--- 03-User-Profile/src/Auth/auth0-variables.js
+++
@@ -1,5 +0,0 @@
-export const AUTH_CONFIG = {
-  clientId: 'GzM1W0ySZe5gSAYg37717ZvESr1aCXCu',
-  domain: 'blogtest.auth0.com',
-  callbackUrl: 'http://localhost:3000/callback'
-}
--- 05-Authorization/src/Auth/auth0-variables.js
+++
@@ -1,6 +0,0 @@
-export const AUTH_CONFIG = {
-  clientId: 'GzM1W0ySZe5gSAYg37717ZvESr1aCXCu',
-  domain: 'blogtest.auth0.com',
-  apiUrl: 'https://api.test.com',
-  callbackUrl: 'http://localhost:3000/callback'
-}
