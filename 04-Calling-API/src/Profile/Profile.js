97eb0def6baed790fe5589dfff549ca65077ac76
Ryan Chenkie <ryanchenkie@gmail.com>
2017-05-01 16:46:16 -0400

    add container

--- 04-Calling-API/src/Profile/Profile.js
+++ 04-Calling-API/src/Profile/Profile.js
@@ -20,16 +20,18 @@ class Profile extends Component {
   render() {
     const { profile } = this.state;
     return (
-      <div className="profile-area">
-        <h1>{profile.name}</h1>
-        <Panel header="Profile">
-          <img src={profile.picture} alt="profile" />
-          <div>
-            <ControlLabel><Glyphicon glyph="user" /> Nickname</ControlLabel>
-            <h3>{profile.nickname}</h3>
-          </div>
-          <pre>{JSON.stringify(profile, null, 2)}</pre>
-        </Panel>
+      <div className="container">
+        <div className="profile-area">
+          <h1>{profile.name}</h1>
+          <Panel header="Profile">
+            <img src={profile.picture} alt="profile" />
+            <div>
+              <ControlLabel><Glyphicon glyph="user" /> Nickname</ControlLabel>
+              <h3>{profile.nickname}</h3>
+            </div>
+            <pre>{JSON.stringify(profile, null, 2)}</pre>
+          </Panel>
+        </div>
       </div>
     );
   }
