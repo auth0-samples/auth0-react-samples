e8bbb782a5e97f622234157e97178e9b01181e40
Reid Peryam <thefastcat@hotmail.com>
2017-11-02 15:09:04 -0500

    facillitation to pass AuthResult vars as props to routed components to encourage Reactjs best practices. Avoids reading AuthResult vars from local storage. Refactors routes.js to a class, moveshandleAuthentication from Auth.js to routes.js.

--- 01-Login/src/routes.js
+++ 01-Login/src/routes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Router } from 'react-router-dom';
 import App from './App';
 import Home from './Home/Home';
@@ -6,25 +6,47 @@ import Callback from './Callback/Callback';
 import Auth from './Auth/Auth';
 import history from './history';
 
-const auth = new Auth();
+export default class Routes extends Component {
+  constructor(){
+    super();
 
-const handleAuthentication = (nextState, replace) => {
-  if (/access_token|id_token|error/.test(nextState.location.hash)) {
-    auth.handleAuthentication();
+    this.auth = new Auth();
+
+    this.state = {
+      user_id: null
+    };
   }
-}
 
-export const makeMainRoutes = () => {
-  return (
-      <Router history={history} component={App}>
-        <div>
-          <Route path="/" render={(props) => <App auth={auth} {...props} />} />
-          <Route path="/home" render={(props) => <Home auth={auth} {...props} />} />
-          <Route path="/callback" render={(props) => {
-            handleAuthentication(props);
-            return <Callback {...props} /> 
-          }}/>
-        </div>
-      </Router>
-  );
+  handleAuthentication = (nextState, replace) => {
+    if (/access_token|id_token|error/.test(nextState.location.hash)) {
+      const { auth0 } = this.auth;
+      auth0.parseHash((err, authResult) => {
+        if (authResult && authResult.accessToken && authResult.idToken) {
+          this.auth.setSession(authResult);
+          // persist logged-in user_id (and/or other variables) in state to pass to child components
+          this.setState({user_id: authResult.idTokenPayload.sub})
+          history.replace('/home');
+        } else if (err) {
+          history.replace('/home');
+          console.log(err);
+          alert(`Error: ${err.error}. Check the console for further details.`);
+        }
+      });
+    }
+  }
+  
+  render() {
+    return (
+        <Router history={history} component={App}>
+          <div>
+            <Route path="/" render={(props) => <App auth={this.auth} {...props} {...this.state} />} />
+            <Route path="/home" render={(props) => <Home auth={this.auth} {...props} {...this.state}/>} />
+            <Route path="/callback" render={(props) => {
+              this.handleAuthentication(props);
+              return <Callback {...props} /> 
+            }}/>
+          </div>
+        </Router>
+    );
+  }
 }
