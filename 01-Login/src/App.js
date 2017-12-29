6040a0c5d4e6925a462b54da7ee91d2e04f4843b
Luciano Balmaceda <balmacedaluciano@gmail.com>
2017-12-29 11:33:23 -0300

    use css id

--- 01-Login/src/App.js
+++ 01-Login/src/App.js
@@ -35,6 +35,7 @@ class App extends Component {
             {
               !isAuthenticated() && (
                   <Button
+                    id="qsLoginBtn"
                     bsStyle="primary"
                     className="btn-margin"
                     onClick={this.login.bind(this)}
@@ -46,6 +47,7 @@ class App extends Component {
             {
               isAuthenticated() && (
                   <Button
+                    id="qsLogoutBtn"
                     bsStyle="primary"
                     className="btn-margin"
                     onClick={this.logout.bind(this)}
