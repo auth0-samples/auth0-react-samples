b0902687ff2bf180e6f759d727df3c63e9f2abe6
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2018-03-20 20:25:28 +0100

    Update login example
    - Auth0.js version bump
    
    - React version bump
    
    - React Bootstrap version bump
    
    - update react-scripts version
    
    - fix warnings

--- 01-Login/src/App.js
+++ 01-Login/src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
         <Navbar fluid>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href="#">Auth0 - React</a>
+              <a href="/">Auth0 - React</a>
             </Navbar.Brand>
             <Button
               bsStyle="primary"
