a6063d76a9c5c536382fbae2a06af28dbb052599
Luke Oliff <luke@lukeoliff.com>
2018-07-06 10:36:09 +0100

    change token storage from localStorage to memory

--- 01-Login/src/Auth/Auth.js
+++ 01-Login/src/Auth/Auth.js
@@ -13,6 +13,8 @@ export default class Auth {
   });
 
   constructor() {
+    this.accessToken = undefined;
+
     this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
     this.handleAuthentication = this.handleAuthentication.bind(this);
@@ -37,28 +39,30 @@ export default class Auth {
   }
 
   setSession(authResult) {
-    // Set the time that the access token will expire at
+    // Set the time that the Access Token will expire at
     let expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
-    localStorage.setItem('access_token', authResult.accessToken);
-    localStorage.setItem('id_token', authResult.idToken);
     localStorage.setItem('expires_at', expiresAt);
+
+    this.accessToken = authResult.accessToken;
+
     // navigate to the home route
     history.replace('/home');
   }
 
   logout() {
-    // Clear access token and ID token from local storage
-    localStorage.removeItem('access_token');
-    localStorage.removeItem('id_token');
+    // Clear stored information
     localStorage.removeItem('expires_at');
+
+    this.accessToken = undefined;
+
     // navigate to the home route
     history.replace('/home');
   }
 
   isAuthenticated() {
-    // Check whether the current time is past the 
-    // access token's expiry time
+    // Check whether the current time is past the
+    // Access Token's expiry time
     let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
-    return new Date().getTime() < expiresAt;
+    return (Date.now() < expiresAt) && this.accessToken;
   }
 }
