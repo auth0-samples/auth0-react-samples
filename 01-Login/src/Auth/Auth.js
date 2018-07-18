bd78469fce8e8ff4379294054bbd545a58ab2c58
Luke Oliff <luke@lukeoliff.com>
2018-07-18 12:15:27 +0100

    renewal in 01-Login

--- 01-Login/src/Auth/Auth.js
+++ 01-Login/src/Auth/Auth.js
@@ -8,7 +8,7 @@ export default class Auth {
     clientID: AUTH_CONFIG.clientId,
     redirectUri: AUTH_CONFIG.callbackUrl,
     audience: `https://${AUTH_CONFIG.domain}/userinfo`,
-    responseType: 'token id_token',
+    responseType: 'token',
     scope: 'openid'
   });
 
@@ -21,6 +21,10 @@ export default class Auth {
     this.handleAuthentication = this.handleAuthentication.bind(this);
     this.isAuthenticated = this.isAuthenticated.bind(this);
     this.goTo = this.goTo.bind(this);
+
+    if (localStorage.getItem('loggedIn') === 'true') {
+      this.renewAuthentication();
+    }
   }
 
   login() {
@@ -29,25 +33,45 @@ export default class Auth {
 
   handleAuthentication() {
     this.auth0.parseHash((err, authResult) => {
-      if (authResult && authResult.accessToken && authResult.idToken) {
-        this.setSession(authResult);
-        this.goTo('/home');
-      } else if (err) {
-        this.goTo('/home');
+      if (err) {
+        this.logout();
+
         console.log(err);
         alert(`Error: ${err.error}. Check the console for further details.`);
+      } else if (authResult && authResult.accessToken) {
+        this.storeToken(authResult);
+        this.goTo('/home');
       }
     });
   }
 
-  setSession(authResult) {
+  renewAuthentication() {
+    this.auth0.checkSession({}, (err, authResult) => {
+        if (err) {
+          this.logout();
+
+          console.log(err);
+          alert(`Could not get a new token (${err.error}: ${err.error_description}).`);
+        } else if (authResult && authResult.accessToken) {
+          this.storeToken(authResult);
+          this.goTo('/home');
+        }
+      }
+    );
+  }
+
+  storeToken(authResult) {
     this.expires = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
     this.accessToken = authResult.accessToken;
+
+    localStorage.setItem('loggedIn', 'true');
   }
 
   logout() {
     this.expires = null;
     this.accessToken = null;
+
+    localStorage.removeItem('loggedIn');
 
     this.goTo('/home');
   }
