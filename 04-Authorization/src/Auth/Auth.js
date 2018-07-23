062b657ceab5e3be87a893c8d860e30842b334c1
Luke Oliff <luke@lukeoliff.com>
2018-07-23 13:01:38 +0100

    wip

--- 04-Authorization/src/Auth/Auth.js
+++ 04-Authorization/src/Auth/Auth.js
@@ -1,22 +1,22 @@
 import auth0 from 'auth0-js';
-import { AUTH_CONFIG } from './auth0-variables';
 import history from '../history';
+import { AUTH_CONFIG } from './auth0-variables';
 
 export default class Auth {
-  auth0 = new auth0.WebAuth({
-    domain: AUTH_CONFIG.domain,
-    clientID: AUTH_CONFIG.clientId,
-    redirectUri: AUTH_CONFIG.callbackUrl,
-    audience: AUTH_CONFIG.apiUrl,
-    responseType: 'token id_token',
-    scope: this.requestedScopes
-  });
-
   expires;
   accessToken;
   userProfile;
   userScopes;
   requestedScopes = 'openid profile read:messages write:messages';
+
+  auth0 = new auth0.WebAuth({
+    domain: AUTH_CONFIG.domain,
+    clientID: AUTH_CONFIG.clientId,
+    redirectUri: AUTH_CONFIG.callbackUrl,
+    audience: AUTH_CONFIG.apiUrl,
+    responseType: 'token',
+    scope: this.requestedScopes
+  });
 
   constructor() {
     this.login = this.login.bind(this);
@@ -27,6 +27,10 @@ export default class Auth {
     this.getAccessToken = this.getAccessToken.bind(this);
     this.getProfile = this.getProfile.bind(this);
     this.goTo = this.goTo.bind(this);
+
+    if (localStorage.getItem('loggedIn') === 'true') {
+      this.renewAuthentication();
+    }
   }
 
   login() {
@@ -35,21 +39,37 @@ export default class Auth {
 
   handleAuthentication() {
     this.auth0.parseHash((err, authResult) => {
-      if (authResult && authResult.accessToken && authResult.idToken) {
-        this.setSession(authResult);
-        this.goTo('/home');
-      } else if (err) {
+      if (err) {
         this.goTo('/home');
         console.log(err);
         alert(`Error: ${err.error}. Check the console for further details.`);
+      } else if (authResult && authResult.accessToken) {
+        this.storeToken(authResult);
+        this.goTo('/home');
       }
     });
   }
 
-  setSession(authResult) {
+  renewAuthentication() {
+    this.auth0.checkSession({}, (err, authResult) => {
+        if (err) {
+          this.logout();
+          console.log(err);
+          alert(`Could not get a new token (${err.error}: ${err.error_description}).`);
+        } else if (authResult && authResult.accessToken) {
+          this.storeToken(authResult);
+          this.goTo('/home');
+        }
+      }
+    );
+  }
+
+  storeToken(authResult) {
     this.expires = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
     this.accessToken = authResult.accessToken;
     this.userScopes = JSON.stringify(authResult.scope || this.requestedScopes || '');
+
+    localStorage.setItem('loggedIn', 'true');
   }
 
   getAccessToken() {
@@ -75,6 +95,8 @@ export default class Auth {
     this.accessToken = null;
     this.userProfile = null;
     this.userScopes = null;
+
+    localStorage.removeItem('loggedIn');
 
     this.goTo('/home');
   }
