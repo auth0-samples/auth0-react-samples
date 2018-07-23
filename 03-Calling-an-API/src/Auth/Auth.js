13bfc124daeaf712423b68d745054d1ae50a9fa6
Luke Oliff <luke@lukeoliff.com>
2018-07-23 12:48:44 +0100

    wip

--- 03-Calling-an-API/src/Auth/Auth.js
+++ 03-Calling-an-API/src/Auth/Auth.js
@@ -8,7 +8,7 @@ export default class Auth {
     clientID: AUTH_CONFIG.clientId,
     redirectUri: AUTH_CONFIG.callbackUrl,
     audience: AUTH_CONFIG.apiUrl,
-    responseType: 'token id_token',
+    responseType: 'token',
     scope: 'openid profile read:messages'
   });
 
