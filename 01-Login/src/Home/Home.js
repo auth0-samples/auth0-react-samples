aa100533787711073ce6c15ca8616d70ce838c1c
benlizar <36707032+benlizar@users.noreply.github.com>
2019-04-23 07:01:24 -0400

    Fix [QS-97] (#115): Vulnerabilities in React QS
    Fix [QS-97]: Vulnerabilities in React QS

--- 01-Login/src/Home/Home.js
+++ 01-Login/src/Home/Home.js
@@ -19,10 +19,8 @@ class Home extends Component {
           !isAuthenticated() && (
               <h4>
                 You are not logged in! Please{' '}
-                <a
-                  style={{ cursor: 'pointer' }}
-                  onClick={this.login.bind(this)}
-                >
+                <a style={{ cursor: 'pointer' }}
+                  onClick={this.login.bind(this)}>
                   Log In
                 </a>
                 {' '}to continue.
