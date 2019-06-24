21f10d8ef50000930e116f4c626ed162cc643e35
Steve Hobbs <steve.hobbs.mail@gmail.com>
2019-06-24 18:10:51 +0100

    Renamed 'redirect_uri' to 'returnTo'

--- 02-Calling-an-API/src/components/NavBar.js
+++ 02-Calling-an-API/src/components/NavBar.js
@@ -29,7 +29,7 @@ const NavBar = () => {
 
   const logoutWithRedirect = () =>
     logout({
-      redirect_uri: window.location.origin
+      returnTo: window.location.origin
     });
 
   return (
