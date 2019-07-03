0490deea7fc47c5483a783d20e7d0422133b9adc
Steve Hobbs <steve.hobbs.mail@gmail.com>
2019-07-03 13:29:40 +0100

    Refactored dependencies and docker build in sample 02

--- 02-Calling-an-API/exec.sh
+++ 02-Calling-an-API/exec.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker build -t auth0-react-03-calling-an-api .
-docker run --init -p 3000:3000 -it auth0-react-03-calling-an-api
+docker build -t auth0-react-02-calling-an-api .
+docker run --init -p 3000:3000 -it auth0-react-02-calling-an-api
