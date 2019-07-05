80476f94813f13376d5cd1d41ee342b28e6bef8a
Steve Hobbs <steve.hobbs@auth0.com>
2019-07-05 10:54:35 +0100

    Updates to the quickstart theme (#132)
    * Added bootstrap assets
    
    * Updated styles for 01-Login
    
    * Updated styles for 02-Calling-an-API

--- 02-Calling-an-API/src/utils/initFontAwesome.js
+++ 02-Calling-an-API/src/utils/initFontAwesome.js
@@ -1,8 +1,10 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { faLink, faPowerOff, faUser } from "@fortawesome/free-solid-svg-icons";
 
 function initFontAwesome() {
   library.add(faLink);
+  library.add(faUser);
+  library.add(faPowerOff);
 }
 
 export default initFontAwesome;
