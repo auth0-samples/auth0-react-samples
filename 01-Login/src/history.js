e9bf419da7ae91f55ca0a7c2c2115dee6fa25606
Steve Hobbs <steve.hobbs.mail@gmail.com>
2019-09-25 12:50:55 +0100

    Update usage of history.js (#119)
    Also ran `npm audit fix` on all samples

--- 01-Login/src/history.js
+++ 01-Login/src/history.js
@@ -1,3 +1,3 @@
-import createHistory from 'history/createBrowserHistory'
+import { createBrowserHistory as createHistory } from "history";
 
-export default createHistory()
+export default createHistory();
