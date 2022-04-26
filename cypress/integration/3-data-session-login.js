7166b3c0112bb6a32aacd81dc1387ffd92dceed5
Murat Ozcan <muratkerem@gmail.com>
2022-04-26 17:56:33 -0500

    data-session

---
+++ cypress/integration/3-data-session-login.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+/* eslint-disable no-undef */
+
+describe("Multi domain login", () => {
+  beforeEach(() =>
+    cy.dataSessionLogin(Cypress.env("EMAIL"), Cypress.env("PASSWORD"))
+  );
+
+  it("Logs in", () => {
+    cy.get("h1").should("contain", "React.js Sample Project");
+  });
+
+  it("Logs in again", () => {
+    cy.contains("React.js Sample Project");
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -15,3 +15,45 @@ Cypress.Commands.add("sessionLogin", (email, password) => {
   });
   return cy.visit("/");
 });
+
+Cypress.Commands.add("dataSessionLogin", (email, password) => {
+  const args = [email, password];
+
+  return cy.dataSession({
+    name: `${email}-${password}`,
+
+    init: () => {
+      cy.log(
+        `**init()**: runs when there is nothing in cache. Yields the value to validate()`
+      );
+    },
+
+    validate: () => {
+      cy.log(`**validate()**`);
+
+      return true;
+    },
+
+    setup: () => {
+      cy.log(`**setup()**`);
+      return cy.origin(Cypress.env("AUTH_URL"), { args }, (args) => {
+        cy.get("input[inputmode='email']").type(args[0]);
+        cy.get("input[type='password']").type(args[1]);
+        cy.contains("button[type='submit']", "Continue").click();
+      });
+    },
+
+    recreate: () => {
+      cy.log(`**recreate()**:`);
+      Promise.resolve();
+    },
+
+    onInvalidated: () => {
+      cy.log(`**onInvalidated**: runs when validate() returns false.
+           Will be called before the "setup" function executes.`);
+      Cypress.clearDataSession(sessionName);
+    },
+
+    shareAcrossSpecs: true,
+  });
+});
