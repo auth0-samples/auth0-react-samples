462808c4e255cf4564e18fce658f5fadcd3ba7e0
Ewan Harris <ewan.harris@okta.com>
2023-03-29 14:35:12 +0100

    Display errors from the API in the UI
    Closes #218

--- Sample-01/api-server.js
+++ Sample-01/api-server.js
@@ -2,7 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
-const { auth } = require("express-oauth2-jwt-bearer");
+const {
+  auth,
+  InvalidTokenError,
+  InvalidRequestError,
+  InsufficientScopeError,
+  requiredScopes,
+} = require("express-oauth2-jwt-bearer");
 const authConfig = require("./src/auth_config.json");
 
 const app = express();
@@ -11,11 +17,7 @@ const port = process.env.API_PORT || 3001;
 const appPort = process.env.SERVER_PORT || 3000;
 const appOrigin = authConfig.appOrigin || `http://localhost:${appPort}`;
 
-if (
-  !authConfig.domain ||
-  !authConfig.audience ||
-  authConfig.audience === "YOUR_API_IDENTIFIER"
-) {
+if (!authConfig.domain || !authConfig.audience || authConfig.audience === "YOUR_API_IDENTIFIER") {
   console.log(
     "Exiting: Please make sure that auth_config.json is in place and populated with valid domain and audience values"
   );
@@ -27,16 +29,35 @@ app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors({ origin: appOrigin }));
 
-const checkJwt = auth({
-  audience: authConfig.audience,
-  issuerBaseURL: `https://${authConfig.domain}/`,
-  algorithms: ["RS256"],
-});
+app.use(
+  auth({
+    audience: authConfig.audience,
+    issuerBaseURL: `https://${authConfig.domain}/`,
+    algorithms: ["RS256"],
+  })
+);
 
-app.get("/api/external", checkJwt, (req, res) => {
+app.get("/api/external", requiredScopes('admin'), (req, res) => {
   res.send({
     msg: "Your access token was successfully validated!",
   });
+});
+
+// Custom error handler that will turn the errors from express-oauth2-jwt-bearer into a JSON object
+// for the UI to handle
+app.use((err, req, res, next) => {
+  if (
+    err instanceof InvalidTokenError ||
+    err instanceof InvalidRequestError ||
+    err instanceof InsufficientScopeError
+  ) {
+    return res.status(err.status).send({
+      error: err.code,
+      message: err.message,
+    });
+  }
+
+  res.send(err);
 });
 
 app.listen(port, () => console.log(`API Server listening on port ${port}`));
