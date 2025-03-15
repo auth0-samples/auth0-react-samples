16d06d1d863ce05cd988b0f01a1f9f8b036e9ac6
DJ Bauch <dj.bauch@gmail.com>
2025-03-15 03:11:15 -0500

    Try trigger on push to default branch

--- Sample-01/server.js
+++ Sample-01/server.js
@@ -1,22 +1,22 @@
-const express = require("express")
-const morgan = require("morgan")
-const helmet = require("helmet")
-const { join } = require("path")
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const { join } = require("path");
 
-const app = express()
+const app = express();
 
-const port = +(process.env.SERVER_PORT ?? '3000')
+const port = +(process.env.SERVER_PORT ?? '3000');
 
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 
 app.use(
   helmet({
     contentSecurityPolicy: false,
   })
-)
+);
 
-app.use(express.static(join(__dirname, "build")))
+app.use(express.static(join(__dirname, "build")));
 
-app.get('*', (req, res) => res.sendFile(join(__dirname, 'build', 'index.html')))
+app.get('*', (req, res) => res.sendFile(join(__dirname, 'build', 'index.html')));
 
-app.listen(port, () => console.log(`Server listening on port ${port}`))
+app.listen(port, () => console.log(`Server listening on port ${port}`));
