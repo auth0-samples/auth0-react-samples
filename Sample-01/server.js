1ed7aec017c51dfef2b30dd0b3adf8833b99fa07
DJ Bauch <dj.bauch@gmail.com>
2025-03-17 21:37:55 -0500

    Fixed type confusion for the environment variables

--- Sample-01/server.js
+++ Sample-01/server.js
@@ -5,7 +5,7 @@ const { join } = require("path");
 
 const app = express();
 
-const port = process.env.SERVER_PORT || 3000;
+const port = +(process.env.SERVER_PORT ?? '3000');
 
 app.use(morgan("dev"));
 
