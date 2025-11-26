3a9573f9897823429349b29448b30b0b78e81667
Nicolas Garcia <nicolas.garcia@okta.com>
2025-11-26 07:16:48 -0300

    Remove index.css to simplify app files and code

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
 /* highlight-start auth0-provider */
 import { Auth0Provider } from "@auth0/auth0-react";
